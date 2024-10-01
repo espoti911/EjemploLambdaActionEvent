43da3baaa4403180adb042909d656aa666724e93
Francisco Manuel Gonzalez Martin <franciscomanuelgonzalezmartin@gmail.com>
2024-10-01 13:46:29 +0200

    Arreglo: el usuario podia introducir letras

--- src/main/java/es/ieslosmontecillos/ejemplolambdaactionevent/HelloApplication.java
+++ src/main/java/es/ieslosmontecillos/ejemplolambdaactionevent/HelloApplication.java
@@ -35,15 +35,15 @@ public class HelloApplication extends Application {
                 };
 
         //Textfields del programa
-        TextField textFieldInterestRate = new TextField();
-        TextField textFieldNYears = new TextField();
-        TextField textFieldLoan = new TextField();
-        TextField textFieldMonthlyPayment = new TextField();
-        TextField textFieldTotalPayment = new TextField();
+        TextField tfInterestRate = new TextField();
+        TextField tfNYears = new TextField();
+        TextField tfLoan = new TextField();
+        TextField tfMonthlyPayment = new TextField();
+        TextField tfTotalPayment = new TextField();
 
         //Desactivamos los textfields de salida
-        textFieldMonthlyPayment.setDisable(true);
-        textFieldTotalPayment.setDisable(true);
+        tfMonthlyPayment.setDisable(true);
+        tfTotalPayment.setDisable(true);
 
         //Texto de error en rojo
         Text errorText = new Text();
@@ -55,10 +55,10 @@ public class HelloApplication extends Application {
         calculateButton.setOnAction(_ -> {
 
             //Comprobamos si los datos que nos han introducido son validos
-            if (validateInput(textFieldInterestRate, textFieldNYears, textFieldLoan))
+            if (validateInput(tfInterestRate, tfNYears, tfLoan))
             {
                 errorText.setText("");
-                calculate(textFieldMonthlyPayment, textFieldTotalPayment);
+                calculate(tfMonthlyPayment, tfTotalPayment);
             }
             else
             {
@@ -78,11 +78,11 @@ public class HelloApplication extends Application {
         }
 
         //Añadimos los textfields
-        root.add(textFieldInterestRate, 1, 0);
-        root.add(textFieldNYears, 1, 1);
-        root.add(textFieldLoan, 1, 2);
-        root.add(textFieldMonthlyPayment, 1, 3);
-        root.add(textFieldTotalPayment, 1, 4);
+        root.add(tfInterestRate, 1, 0);
+        root.add(tfNYears, 1, 1);
+        root.add(tfLoan, 1, 2);
+        root.add(tfMonthlyPayment, 1, 3);
+        root.add(tfTotalPayment, 1, 4);
 
         //Añadimos el boton
         root.add(calculateButton, 1, 5);
@@ -100,20 +100,25 @@ public class HelloApplication extends Application {
 
     /**
      * Metodo que comprueba si los datos que nos ha introducido el usuario son validos
-     * @param textFieldInterestRate textfield con el ratio de interes
-     * @param textFieldNYears textfield con los numeros de años
-     * @param textFieldLoan textfield con la cantidad del prestamo
+     * @param tfInterestRate textfield con el ratio de interes
+     * @param tfNYears textfield con los numeros de años
+     * @param tfLoan textfield con la cantidad del prestamo
      * @return True: Los datos son validos, False: hay algun dato que no es valido
      */
-    private boolean validateInput(TextField textFieldInterestRate,
-                               TextField textFieldNYears,
-                               TextField textFieldLoan)
+    private boolean validateInput(TextField tfInterestRate,
+                               TextField tfNYears,
+                               TextField tfLoan)
     {
+        if (!tfInterestRate.getText().trim().matches("^[0-9]+$")) return false;
+        if (!tfNYears.getText().trim().matches("^[0-9]+$")) return false;
+        if (!tfLoan.getText().trim().matches("^[0-9]+$")) return false;
+
         try
         {
-            interestRate = Double.parseDouble(textFieldInterestRate.getText());
-            nYears = Integer.parseInt(textFieldNYears.getText());
-            loan = Double.parseDouble(textFieldLoan.getText());
+            interestRate = Double.parseDouble(tfInterestRate.getText().trim());
+            nYears = Integer.parseInt(tfNYears.getText().trim());
+            loan = Double.parseDouble(tfLoan.getText().trim());
+
         }
         catch (Exception e)
         {
@@ -124,16 +129,16 @@ public class HelloApplication extends Application {
 
     /**
      * Metodo que calcula el pago total y mensual segun los datos introducidos
-     * @param textFieldMonthlyPayment textfield de salida donde se mostrara el pago mensual
-     * @param textFieldTotalPayment textfield de salida donde se mostrara el pago total
+     * @param tfMonthlyPayment textfield de salida donde se mostrara el pago mensual
+     * @param tfTotalPayment textfield de salida donde se mostrara el pago total
      */
-    private void calculate(TextField textFieldMonthlyPayment, TextField textFieldTotalPayment)
+    private void calculate(TextField tfMonthlyPayment, TextField tfTotalPayment)
     {
         double r = interestRate / 100 * 12;
         double m = loan * r / (1 - Math.pow(1 + r, -12 * nYears));
 
-        textFieldMonthlyPayment.setText(String.format("%.2f €", m));
-        textFieldTotalPayment.setText(String.format("%.2f €", m * 12 * nYears));
+        tfMonthlyPayment.setText(String.format("%.2f €", m));
+        tfTotalPayment.setText(String.format("%.2f €", m * 12 * nYears));
     }
 
     public static void main(String[] args) {
